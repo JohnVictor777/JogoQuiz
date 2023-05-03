871bc24bcda78f6e879043bba8a45e5cc8cea5fd
John Victor <victorjohn609@gmail.com>
2023-05-03 16:10:41 -0300

    Jogo quiz

---
+++ questions.js
@@ -0,0 +1,153 @@
+let questions = [
+    {
+        question: 'Qual desses desenhos foi criado primeiro?',
+        options: [
+            'Mickey',
+            'Popeye',
+            'Pantera-cor-de-rosa',
+            'Pica-pau'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual é o nome do cachorro do Cebolinha?',
+        options: [
+            'floquinho',
+            'verdinho',
+            'algodão',
+            'doquinho'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual é o nome do dono do Snoopy?',
+        options: [
+            'Jack Lee',
+            'Charlie Brown',
+            'Harry Brown',
+            'Cebolinha'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual é o nome do melhor amigo de Bart?',
+        options: [
+            'Van Milhouse',
+            'Ryan Van Black Houten',
+            'Will Van Houten',
+            'Milhouse Van Houten'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Quem é o melhor amigo de Gumball?',
+        options: [
+            'Darwin',
+            'Watterson',
+            'Darfinn',
+            'Daphene'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Em Coragem o cão covarde quem é Katz?',
+        options: [
+            'Um cachorro',
+            'O dono do Coragem',
+            'Um gato',
+            'Um fantasma'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Em Pokémon, qual é o nome do dono do Pikachu?',
+        options: [
+            'Fuyuki',
+            'Ash',
+            'Ketchum',
+            'Brock'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual país criou o Irmão do Jorel?',
+        options: [
+            'Espanha',
+            'Estados Unidos',
+            'Brasil',
+            'Mexico'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quais os nomes dos 2 personagens principais que tem uma música de abertura que o nome é pegue o pombo?',
+        options: [
+            'Dick Vigarista e Muttley',
+            'Dick Vigarista e Rank',
+            'Os Simpsons',
+            'Pica-Pau'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual foi o primeiro desenho a ser exibido no brasil?',
+        options: [
+            'Popeye',
+            'Mickey',
+            'Pica-Pau',
+            'Bananas de pijama'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual é o personagem mais rico dos desenhos animados?',
+        options: [
+            'Bruce Wayne',
+            'Smaug',
+            'Tony Stark',
+            'Tio Patinhas'
+        ],
+        answer: 3
+    },
+    {
+        question: 'O Scooby-doo é que grau parentesco do Scooby-loo?',
+        options: [
+            'Ele é primo do Scooby-loo',
+            'Ele é pai do Scooby-loo',
+            'Ele é tio do Scooby-loo',
+            'Ele é irmão do Scooby-loo'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual é o nome do gato preto que ajuda Coraline?',
+        options: [
+            'Conner',
+            'Gato',
+            'John',
+            'Black'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual é o nome do principal vilão de Popeye?',
+        options: [
+            'Brutus',
+            'Rambo',
+            'Looney',
+            'Bruto'
+
+        ],
+        answer: 0
+    },
+    {
+        question: 'Em qual cidade vivem Os Simpsons?',
+        options: [
+            'Valença',
+            'Nova Iorque',
+            'Springfield',
+            'Konoha'
+        ],
+        answer: 2
+    },
+];
